46cb338df490134cc2ab2230beab76d9146540eb
vishal nashani <vishalnashani24@gmail.com>
2016-04-23 11:29:41 +0530

    change

--- app/controllers/holiday.server.controller.js
+++ app/controllers/holiday.server.controller.js
@@ -6,7 +6,7 @@
 var mongoose = require('mongoose'),
   errorHandler = require('./errors.server.controller'),
     Holiday = mongoose.model('Holiday'),
-    // multer  =   require('multer'),
+     multer  =   require('multer'),
     _ = require('lodash');
 
 /**
@@ -14,9 +14,9 @@ var mongoose = require('mongoose'),
  */
 exports.create = function(req, res) {
     //holiday store ten api start
-        /*var multer  =   require('multer');
-        var fs = require('fs');
-        var csv = require('fast-csv');
+        //var multer  =   require('multer');
+        //var fs = require('fs');
+        //var csv = require('fast-csv');
         var storage =   multer.diskStorage({
             destination: function (req, file, callback) {
             callback(null, './uploads');
@@ -25,64 +25,65 @@ exports.create = function(req, res) {
             callback(null, file.originalname );
             }
             });
-        var upload = multer({ storage : storage}).single('userPhoto');*/
+        var upload = multer({ storage : storage}).single('userPhoto');
+        res.json('file uploaded');
 
-        /*router.get('/',function(req,res){
-        res.sendFile(__dirname + "/index.html");
-        });*/
+    //     /*router.get('/',function(req,res){
+    //     res.sendFile(__dirname + "/index.html");
+    //     });*/
 
-        /*router.post('/holiday',function(req,res){
-        var dataArr = [];
-        upload(req,res,function(err) {
-        if(err) {
-            return res.end("Error uploading file.");
-        }
-        //res.end("File is uploaded");
-        fs.createReadStream('./uploads/holiday.csv')
-        .pipe(csv())
-        .on('data',function(data){
-            console.log("datais",data);
-            var json ={
-                    date:data[0],
-                    day:data[1],
-                    holidayname:data[2]
+    //    // router.post('/holiday',function(req,res){
+    //     var dataArr = [];
+    //     upload(req,res,function(err) {
+    //     if(err) {
+    //         return res.end("Error uploading file.");
+    //     }
+    //     //res.end("File is uploaded");
+    //     fs.createReadStream('./uploads/holiday.csv')
+    //     .pipe(csv())
+    //     .on('data',function(data){
+    //         console.log("datais",data);
+    //         var json ={
+    //                 date:data[0],
+    //                 day:data[1],
+    //                 holidayname:data[2]
 
-                }
+    //             }
 
-                //console.log(json);
-                dataArr.push(json);
+    //             //console.log(json);
+    //             dataArr.push(json);
             
-        })
-        .on('end',function(data){
-            console.log("value is",data);
-            saveData(0);
-        })
+    //     })
+    //     .on('end',function(data){
+    //         console.log("value is",data);
+    //         saveData(0);
+    //     })
 
 
 
-    });
-    function saveData(argument) {
-        //console.log('dataArr',dataArr);
-        var i =argument;
-        if (i<dataArr.length) {
-            console.log(i,dataArr[i]);
-            holidaydata.create(dataArr[i],function(err,result){
-                if(err){
-                    console.log(err);
-                }else{
-                    console.log(result+" "+i);
-                    i++;
-                    saveData(i);
-                }
+    // });
+    // function saveData(argument) {
+    //     //console.log('dataArr',dataArr);
+    //     var i =argument;
+    //     if (i<dataArr.length) {
+    //         console.log(i,dataArr[i]);
+    //         holidaydata.create(dataArr[i],function(err,result){
+    //             if(err){
+    //                 console.log(err);
+    //             }else{
+    //                 console.log(result+" "+i);
+    //                 i++;
+    //                 saveData(i);
+    //             }
 
-            });
+    //         });
             
-        }else{
-            res.send("data save");
-        }
+    //     }else{
+    //         res.send("data save");
+    //     }
         
-    }
-    });  */ //holiay store ten api end
+    // }
+    // });  */ //holiay store ten api end
 
 
 };
